bea58de86b0792c47948246eef02f08cf4db08be
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-03-01 20:08:33 -0800

    Add javadoc comments

--- src/address/DatabaseConnect.java
+++ src/address/DatabaseConnect.java
@@ -2,7 +2,20 @@ package address;
 
 import java.sql.*;
 
+/**
+ * Class to connect to database containing
+ * AddressBook entries
+ *
+ * @author Tey Jon Sornet
+ * @since March 2021
+ */
 public class DatabaseConnect {
+    /**
+     * Main method that runs the application
+     * @param args String arguments
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // Load Oracle JDBC Driver
         Class.forName("oracle.jdbc.OracleDriver");
@@ -19,10 +32,11 @@ public class DatabaseConnect {
 
         // Iterate through result and print employee names
         while (rset.next()) {
-            // Visit each column
+            // Visit each column and then print
             for (int i = 1; i <= rset.getMetaData().getColumnCount(); i++) {
                 System.out.print(rset.getString(i).trim() + " | ");
             }
+            // Next line and print horizontal divider
             System.out.println("");
             System.out.println("************************");
         }
